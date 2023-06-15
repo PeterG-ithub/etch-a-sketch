79eeb2a8375eb03e0a43b694253f967f12e7963e
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-14 18:43:19 -0700

    Add function createGrid.
    Added function createGrid in main.js.
    This function will create divs which is in
    a for loop. The divs are styled in css.
    Edited html classes to more descriptive
    name.

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello There!");
+const gridContainer = document.querySelector('.grid-container')
+
+function createGrid() {
+    console.log("Hello there!")
+    for (let i = 0; i < 25; i++)
+    {
+        const div = document.createElement('div');
+        gridContainer.appendChild(div);
+        console.log("Hello ");
+    }
+    
+}
+
+createGrid();
