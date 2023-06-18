ba93c9f595e2adad0964221b02de6b4ccd345791
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-17 18:09:08 -0700

    Add function createGrid() with hover effect
    This function will create a certain number
    of div depending on the function arguments.
    Thsi function calculates the width and heigth
    for the divs. this function also adds an
    eventlistener to each div so that when is hovered,
    a class called "test" will be added to the div,
    which will change the background color.

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const gridContainer = document.querySelector('.grid-container')
 
-function createGrid() {
-    console.log("Hello there!")
-    for (let i = 0; i < 25; i++)
+function createGrid(size) {
+    let divSize = 500/size;
+    console.log(divSize)
+    for (let i = 0; i < size*size; i++)
     {
         const div = document.createElement('div');
+        div.style.width = divSize + "px";
+        div.style.height = divSize + "px";
+        div.addEventListener('mouseover', () => {
+            div.classList.add('test');
+        });
         gridContainer.appendChild(div);
         console.log("Hello ");
     }
     
 }
 
-createGrid();
+createGrid(25);
