c626e1797a67db4c86ed8971f61df3c1eb2f36aa
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-18 19:19:39 -0700

    Add resetBoard function and btn:hover interaction
    resetBoard function will delete the class hovered.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const gridContainer = document.querySelector('.grid-container')
 const sizeBtn = document.querySelector('.size-btn');
 const eraseBtn = document.querySelector('.erase-btn');
 
+eraseBtn.addEventListener('click', () => resetBoard());
+sizeBtn.addEventListener('click', () => promptSize());
 function createGrid(width) {
     let divSize = 900/width;
     let numOfRows = Math.round(width/1.8);
@@ -12,11 +14,18 @@ function createGrid(width) {
         div.style.width = `${divSize}px`;
         div.style.height = divSize + "px";
         div.addEventListener('mouseover', () => {
-            div.classList.add('test');
+            div.classList.add('hovered');
         });
         gridContainer.appendChild(div);
     }
     gridContainer.style.height = divSize*numOfRows + "px";
+}
+
+function resetBoard() {
+    const hovered = document.querySelectorAll(".hovered");
+    hovered.forEach((element) => {
+        element.classList.remove('hovered');
+    });
 }
 
 createGrid(81);
