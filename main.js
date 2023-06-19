e23862ea3e616fedeecd8cd5a004fe0f969e1841
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-18 18:56:34 -0700

    Fix createGrid() and added borders
    Fixed the createGrid function to generate
    the correct number of pixels/cells.

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
 const gridContainer = document.querySelector('.grid-container')
+const sizeBtn = document.querySelector('.size-btn');
+const eraseBtn = document.querySelector('.erase-btn');
 
-function createGrid(size) {
-    let divSize = 500/size;
-    console.log(divSize)
-    for (let i = 0; i < size*size; i++)
+function createGrid(width) {
+    let divSize = 900/width;
+    let numOfRows = Math.round(width/1.8);
+    console.log(numOfRows);
+    for (let i = 0; i < width*numOfRows; i++)
     {
         const div = document.createElement('div');
-        div.style.width = divSize + "px";
+        div.style.width = `${divSize}px`;
         div.style.height = divSize + "px";
         div.addEventListener('mouseover', () => {
             div.classList.add('test');
         });
         gridContainer.appendChild(div);
-        console.log("Hello ");
     }
-    
+    gridContainer.style.height = divSize*numOfRows + "px";
 }
 
-createGrid(25);
+createGrid(81);
